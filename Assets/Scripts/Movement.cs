8a0432b49375766c807a02b32fd47bfa86d8487b
JGale311 <justingale92@gmail.com>
2016-02-23 23:19:30 -0500

    Fixed Initial

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+
+	public bool controllable = true;
+
+	public float speed = 7.0f;
+	public float jumpSpeed = 6.0f;
+	public float gravity = 20.0f;
+
+	private Vector3 moveDirection = Vector3.zero;
+	private CharacterController controller;
+
+	void Start()
+	{
+		controller = GetComponent<CharacterController> ();
+	}
+
+	void Update()
+	{
+		if (controller.isGrounded && controllable)
+
+		{
+			moveDirection = new Vector3 (Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical"));
+			moveDirection = transform.TransformDirection (moveDirection);
+			moveDirection *= speed;
+
+			if (Input.GetButton ("Jump"))
+				moveDirection.y = jumpSpeed;
+
+		}
+
+
+		moveDirection.y -= gravity * Time.deltaTime;
+		controller.Move (moveDirection * Time.deltaTime);
+	}
+
+}
---
+++ Assets/Scripts/Objects/DeviceOperator.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeviceOperator : MonoBehaviour {
+
+
+	public float radius = 1.5f;
+
+	void Update() 
+	{
+		if(Input.GetKeyDown(KeyCode.Mouse0))
+		{
+			Ray ray = new Ray (transform.position, transform.forward);
+			RaycastHit hit;
+			if(Physics.Raycast(ray, out hit, radius))
+			{
+				if(hit.collider.CompareTag("Door"))
+				{
+					hit.collider.transform.parent.GetComponent<DoorOpen>().ChangeDoorState();
+
+				
+				
+
+				}
+			}
+		
+	
+}
+}
+}
