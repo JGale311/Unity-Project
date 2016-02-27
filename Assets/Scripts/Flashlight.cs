c8bb9b2503e256b4cb1696d62a98a96659d3b8af
JGale311 <justingale92@gmail.com>
2016-02-26 21:33:29 -0500

    Scripts and Trees
    Flashlight and mouse look script, large tree model

---
+++ Assets/Scripts/Flashlight.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Flashlight : MonoBehaviour {
+
+	Light myLight;
+	bool on = false;
+
+	void Start () {
+
+		myLight = GetComponent<Light> ();
+	}
+
+	void Update()
+
+	{ 
+		if(on)
+			myLight.enabled = true;
+		else if(!on)
+			myLight.enabled = false;
+		
+		if (Input.GetKeyDown (KeyCode.F))
+
+			on = !on;
+
+	}
+
+}
